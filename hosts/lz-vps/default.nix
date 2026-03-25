296d7f3a30ff093dc8b15d90f58a5447f08c4faf
Jaanai-Liu <liujaanai@gmail.com>
2026-03-25 23:48:16 +0800

    secret vps

--- hosts/lz-vps/default.nix
+++ hosts/lz-vps/default.nix
@@ -8,11 +8,11 @@
 
 {
   imports = [
-    (modulesPath + "/profiles/qemu-guest.nix") # 专门给 KVM 虚拟机的驱动
-    ./disk-config.nix # 引入刚才的硬盘规划
+    (modulesPath + "/profiles/qemu-guest.nix")
+    ./disk-config.nix
+    ../../secrets/nixos.nix
   ];
 
-  # 引导程序配置 (兼容双模式)
   boot.loader.grub = {
     enable = true;
     efiSupport = true;
@@ -20,29 +20,23 @@
     device = "/dev/vda";
   };
 
-  # 基础网络与时间
   networking.hostName = "lz-vps";
   time.timeZone = "Asia/Shanghai";
 
-  # 🌟 救命神器：开启 1GB 的 ZRAM 内存压缩，代替硬盘 Swap，让你的 VPS 飞起来
   zramSwap.enable = true;
   zramSwap.memoryPercent = 100;
 
-  # BBR 网络加速
   boot.kernel.sysctl = {
     "net.core.default_qdisc" = "fq";
     "net.ipv4.tcp_congestion_control" = "bbr";
   };
 
-  # 必要的系统包
   environment.systemPackages = with pkgs; [
     wget
     curl
     git
   ];
 
-  # 🌟【必须要改这里！】填入你台式机的 SSH 公钥
-  # 如果你不知道公钥是什么，可以在台式机的终端里运行 `cat ~/.ssh/id_ed25519.pub` 或 `cat ~/.ssh/id_rsa.pub`
   users.users.root = {
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL8B31wZficBJy4Tli3w+C0hsa7uhsMlff43JF6PSYBe liujaanai@gmail.com"
@@ -59,6 +53,7 @@
   networking.firewall.allowedUDPPorts = [ 443 ];
 
   # 启用并配置 Sing-box (VLESS-REALITY)
+  modules.secrets.server.proxy.enable = true;
   services.sing-box = {
     enable = true;
     settings = {
@@ -70,7 +65,7 @@
           listen_port = 443;
           users = [
             {
-              uuid = "cd85c279-474c-4cb2-afec-36bd72a69404";
+              uuid._secret = config.age.secrets."sing-box-uuid".path;
               flow = "xtls-rprx-vision";
             }
           ];
@@ -83,8 +78,10 @@
                 server = "www.microsoft.com";
                 server_port = 443;
               };
-              private_key = "IIMEFCbo7r6oR6zaIrTpgV30RpftKnzsf-dpVcV83X8";
-              short_id = [ "648083e2b1791469" ];
+              private_key._secret = config.age.secrets."sing-box-private-key".path;
+              short_id = [
+                { _secret = config.age.secrets."sing-box-short-id".path; }
+              ];
             };
           };
         }
--- secrets/nixos.nix
+++ secrets/nixos.nix
@@ -11,14 +11,6 @@ with lib;
 let
   cfg = config.modules.secrets;
 
-  enabledServerSecrets =
-    cfg.server.application.enable
-    || cfg.server.network.enable
-    || cfg.server.operation.enable
-    || cfg.server.kubernetes.enable
-    || cfg.server.webserver.enable
-    || cfg.server.storage.enable;
-
   noaccess = {
     mode = "0000";
     owner = "root";
@@ -39,56 +31,78 @@ in
 
   options.modules.secrets = {
     desktop.enable = mkEnableOption "NixOS Secrets for Desktops";
+    server.proxy.enable = mkEnableOption "NixOS Secrets for Proxy Server";
   };
 
-  config = mkIf cfg.desktop.enable {
-    # 安装 agenix 命令行工具，方便你在终端编辑加密文件
-    environment.systemPackages = [
-      agenix.packages."${pkgs.stdenv.hostPlatform.system}".default
-    ];
-
-    # 指定解密用的“钥匙”路径。对于普通系统，使用机器自身的 SSH 主机密钥
-    age.identityPaths = [
-      "/etc/ssh/ssh_host_ed25519_key"
-    ];
-
-    age.secrets = {
-      # 定义你的私钥。file 路径指向你从 GitHub 下载的仓库
-      "ssh-key.age" = {
-        file = "${mysecrets}/secrets/ssh-key.age"; # 假设你存放在这个位置
-      }
-      // user_readable;
-      "github-token" = {
-        file = "${mysecrets}/secrets/github-token.age";
-      }
-      // high_security;
-    };
+  config = mkMerge [
 
     # ==========================================
-    # 🌟 2. 将解密后的 Token 注入 Nix 系统配置
+    # 🖥️ 【台式机专区】只有开启 desktop.enable 才加载
     # ==========================================
-    # 我们使用 !include 语法，这样 Token 内容永远不会进入 /nix/store
-    nix.extraOptions = ''
-      !include ${config.age.secrets."github-token".path}
-    '';
+    (mkIf cfg.desktop.enable {
+      environment.systemPackages = [
+        agenix.packages."${pkgs.stdenv.hostPlatform.system}".default
+      ];
 
-    # 将解密后的私钥软链接到 /etc/ssh 或其它你想要的地方（可选）
-    # 通常 SSH 会自动寻找 ~/.ssh/，你也可以在 home-manager 里处理软链接
-    environment.etc = {
-      "agenix/id_ed25519" = {
-        source = config.age.secrets."ssh-key.age".path;
-        mode = "0500";
-        user = myvars.username;
+      age.identityPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+
+      age.secrets = {
+        "ssh-key.age" = {
+          file = "${mysecrets}/secrets/ssh-key.age";
+        }
+        // user_readable;
+
+        "github-token" = {
+          file = "${mysecrets}/secrets/github-token.age";
+        }
+        // high_security;
+
+        "rclone-alist" = {
+          file = "${mysecrets}/secrets/rclone-alist.age";
+          path = "/home/${myvars.username}/.config/rclone/rclone.conf";
+          owner = myvars.username;
+          group = "users";
+          mode = "0500";
+        };
       };
-    };
 
-    age.secrets."rclone-alist" = {
-      file = "${mysecrets}/secrets/rclone-alist.age";
-      path = "/home/${myvars.username}/.config/rclone/rclone.conf";
-      owner = myvars.username;
-      group = "users";
-      mode = "0500";
-    };
-  };
+      nix.extraOptions = ''
+        !include ${config.age.secrets."github-token".path}
+      '';
+
+      environment.etc = {
+        "agenix/id_ed25519" = {
+          source = config.age.secrets."ssh-key.age".path;
+          mode = "0500";
+          user = myvars.username;
+        };
+      };
+    })
+
+    # ==========================================
+    # ☁️ 【VPS 代理专区】只有开启 server.proxy.enable 才加载
+    # ==========================================
+    (mkIf cfg.server.proxy.enable {
+      # VPS 解密也是用它自己的系统级 SSH 私钥
+      age.identityPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+
+      age.secrets = {
+        "sing-box-uuid" = {
+          file = "${mysecrets}/secrets/sing-box-uuid.age";
+        }
+        // high_security;
+
+        "sing-box-private-key" = {
+          file = "${mysecrets}/secrets/sing-box-private-key.age";
+        }
+        // high_security;
+
+        "sing-box-short-id" = {
+          file = "${mysecrets}/secrets/sing-box-short-id.age";
+        }
+        // high_security;
+      };
+    })
+
+  ];
 }
-
