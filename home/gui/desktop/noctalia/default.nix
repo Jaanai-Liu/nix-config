ae497a5333d4df9febc339e03350a6651cedaa8f
Jaanai-Liu <liujaanai@gmail.com>
2026-03-27 23:48:19 +0800

    update noctalia

--- home/gui/desktop/noctalia/default.nix
+++ home/gui/desktop/noctalia/default.nix
@@ -1,15 +1,20 @@
-{ pkgs, config, inputs, ... }:
+{
+  pkgs,
+  config,
+  inputs,
+  ...
+}:
 let
-  # 使用你自己的 Flake 输入获取 Noctalia
   noctalia-pkg = inputs.noctalia.packages.${pkgs.system}.default;
 in
 {
   home.packages = [
     noctalia-pkg
-    pkgs.qt6Packages.qt6ct # 极其重要：用于统一 Qt 程序的图标和主题（Noctalia 是基于 Qt 的）
-    pkgs.app2unit          # 允许将桌面程序作为 Systemd 服务启动
-  ] ++ (pkgs.lib.optionals pkgs.stdenv.isx86_64 [
-    pkgs.gpu-screen-recorder # 一个非常轻量级的 Wayland 录屏工具
+    pkgs.qt6Packages.qt6ct
+    pkgs.app2unit
+  ]
+  ++ (pkgs.lib.optionals pkgs.stdenv.isx86_64 [
+    pkgs.gpu-screen-recorder
   ]);
 
   xdg.configFile =
@@ -32,9 +37,8 @@ in
 
     Service = {
       ExecStart = "${pkgs.lib.getExe noctalia-pkg}";
-      Restart = "on-failure"; # 如果崩溃了，系统会自动帮你拉起来！
+      Restart = "on-failure";
 
-      # 注入关键的环境变量，修复 2K 屏缩放和渲染 Bug
       Environment = [
         "QT_QPA_PLATFORM=wayland;xcb"
         "QT_QPA_PLATFORMTHEME=qt6ct"
@@ -42,8 +46,6 @@ in
       ];
     };
 
-    Install = {
-      WantedBy = [ "wayland-session.target" ];
-    };
+    Install.WantedBy = [ "wayland-session.target" ];
   };
 }
