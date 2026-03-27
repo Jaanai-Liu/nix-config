4f69c8390ef51c011fb741fd5f55c600aebc7624
Jaanai-Liu <liujaanai@gmail.com>
2026-03-27 11:27:36 +0800

    change udiskie fix

--- modules/core/udiskie.nix
+++ modules/core/udiskie.nix
@@ -1,25 +1,12 @@
 {
   pkgs,
-  lib,
   myvars,
   ...
 }:
 
-let
-  udiskie-with-icons = pkgs.symlinkJoin {
-    name = "udiskie-with-icons";
-    paths = [ pkgs.udiskie ];
-    nativeBuildInputs = [ pkgs.makeWrapper ];
-    postBuild = ''
-      wrapProgram $out/bin/udiskie \
-        --set XDG_DATA_DIRS "${pkgs.papirus-icon-theme}/share:$XDG_DATA_DIRS" \
-        --set GTK_ICON_THEME "Papirus-Dark" \
-        --add-flags "--notify" 
-    '';
-  };
-in
 {
   services.udisks2.enable = true;
+
   boot.supportedFilesystems = [
     "ntfs"
     "exfat"
@@ -28,37 +15,25 @@ in
   environment.systemPackages = [
     pkgs.ntfs3g
     pkgs.exfat
-    pkgs.papirus-icon-theme
-    udiskie-with-icons
   ];
 
+  security.polkit.enable = true;
   security.polkit.extraConfig = ''
     polkit.addRule(function(action, subject) {
-        var YES = polkit.Result.YES;
-        var permission = [
-            "org.freedesktop.udisks2.filesystem-mount",
-            "org.freedesktop.udisks2.filesystem-mount-system",
-            "org.freedesktop.udisks2.eject-media",
-            "org.freedesktop.udisks2.power-off-drive"
-        ];
-        if (permission.indexOf(action.id) > -1 && subject.user == "${myvars.username}") {
-            return YES;
-        }
+      if (subject.isInGroup("wheel") && action.id.startsWith("org.freedesktop.udisks2.")) {
+        return polkit.Result.YES;
+      }
     });
   '';
 
+  # ==========================================
+  # --- Home Manager ---
+  # ==========================================
   home-manager.users.${myvars.username} = {
-    home.sessionVariables = {
-      XDG_DATA_DIRS = "$HOME/.nix-profile/share:$XDG_DATA_DIRS";
-    };
-
     services.udiskie = {
       enable = true;
-      package = udiskie-with-icons;
-      tray = "auto";
+      automount = true;
       notify = true;
     };
-
-    home.packages = [ pkgs.papirus-icon-theme ];
   };
 }
