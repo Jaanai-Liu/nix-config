4bd93284e8ac75ade578a9977491d15f512f6085
Jaanai-Liu <liujaanai@gmail.com>
2026-03-30 01:00:43 +0800

    add shell to vps

--- vars/networking.nix
+++ vars/networking.nix
@@ -1,37 +1,33 @@
 # vars/networking.nix
 { lib }:
 let
-  # 1. 你的设备地址簿
-  hostsAddr = {
-    lz-pc = {
-      ipv4 = "100.81.104.63";
-      user = "myvars.username";
-    };
-    lz-nb = {
-      ipv4 = "100.x.y.z";
-      user = "myvars.username";
-    };
-    lz-vps = {
-      ipv4 = "23.95.28.22";
-      user = "root";
-    };
-    lz-vps-home = {
-      ipv4 = "23.95.28.22";
-      user = "myvars.username";
-    };
+  sshDefault = {
+    port = 22;
+    user = "zheng";
   };
 in
-{
-  inherit hostsAddr;
+rec {
+  hostsAddr = {
+    lz-pc.ipv4 = "100.81.104.63";
+    lz-nb.ipv4 = "100.x.y.z";
+    lz-vps = {
+      ipv4 = "23.95.28.22";
+      ssh.user = "root";
+    };
+    lz-vps-home.ipv4 = "23.95.28.22";
+  };
 
   sshExtraConfig = lib.attrsets.foldlAttrs (
     acc: host: val:
+    let
+      ssh = sshDefault // (if val ? ssh then val.ssh else { });
+    in
     acc
     + ''
       Host ${host}
         HostName ${val.ipv4}
-        User ${val.user}
-        # 如果以后改了 SSH 端口，可以在这里加 Port 字段
+        Port ${toString (ssh.port or 22)}
+        User ${ssh.user}
     ''
   ) "" hostsAddr;
 }
