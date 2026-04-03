e01a7303faf0d111bcd75748d5e754cc4615ecd6
Jaanai-Liu <liujaanai@gmail.com>
2026-04-03 10:37:48 +0800

    firefox default

--- home/gui/browse.nix
+++ home/gui/browse.nix
@@ -8,4 +8,15 @@
   home.sessionVariables = {
     BROWSER = "firefox";
   };
+
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "text/html" = "firefox.desktop";
+      "x-scheme-handler/http" = "firefox.desktop";
+      "x-scheme-handler/https" = "firefox.desktop";
+      "x-scheme-handler/about" = "firefox.desktop";
+      "x-scheme-handler/unknown" = "firefox.desktop";
+    };
+  };
 }
