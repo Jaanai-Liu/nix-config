7d894a5a92885088ecd476853254ad70e5c6ab46
Jaanai-Liu <liujaanai@gmail.com>
2026-04-03 10:33:12 +0800

    yazi as file manager

--- home/tui/yazi.nix
+++ home/tui/yazi.nix
@@ -1,7 +1,9 @@
-# modules/yazi.nix
+# home/tui/yazi.nix
 { pkgs, ... }:
 {
-  # terminal file manager
+  # ==========================================
+  # 🌟 Yazi 本体配置
+  # ==========================================
   programs.yazi = {
     enable = true;
     package = pkgs.yazi;
@@ -14,6 +16,36 @@
         show_hidden = true;
         sort_dir_first = true;
       };
+    };
+  };
+
+  # ==========================================
+  # 🌟 创建桌面启动项 (等同于写 .desktop 文件)
+  # ==========================================
+  xdg.desktopEntries.yazi-folder = {
+    name = "Yazi";
+    comment = "Terminal file manager";
+    icon = "system-file-manager";
+
+    # exec = "kitty yazi %u";
+    exec = "${pkgs.kitty}/bin/kitty ${pkgs.yazi}/bin/yazi %U";
+
+    categories = [
+      "System"
+      "FileTools"
+      "FileManager"
+      "ConsoleOnly"
+    ];
+    mimeType = [ "inode/directory" ];
+  };
+
+  # ==========================================
+  # 🌟 绑定为系统默认文件夹管理器
+  # ==========================================
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "inode/directory" = [ "yazi-folder.desktop" ];
     };
   };
 }
--- modules/desktop/portal.nix
+++ modules/desktop/portal.nix
@@ -1,12 +1,38 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
+let
+  yazi-wrapper = pkgs.writeShellScript "yazi-wrapper" ''
+    #!/usr/bin/env bash
+    out_path="$3"
+    start_dir="$2"
+    ${pkgs.kitty}/bin/kitty --class "yazi-picker" -e ${pkgs.yazi}/bin/yazi --chooser-file="$out_path" "$start_dir"
+  '';
+in
 {
+  environment.etc."xdg/xdg-desktop-portal-termfilechooser/config".text = ''
+    [filechooser]
+    cmd=${yazi-wrapper}
+  '';
+
   xdg.portal = {
     enable = true;
-    extraPortals = [
-      pkgs.xdg-desktop-portal-gtk
-      pkgs.xdg-desktop-portal-gnome
+
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-gtk
+      xdg-desktop-portal-termfilechooser
+      xdg-desktop-portal-gnome
     ];
-    config.common.default = [ "gtk" ];
+
+    config = {
+      common = {
+        default = [ "gtk" ];
+      };
+
+      niri = {
+        # default = lib.mkForce [ "gtk" ];
+        default = lib.mkForce [ "termfilechooser" ];
+        "org.freedesktop.impl.portal.ScreenCast" = [ "gnome" ];
+        "org.freedesktop.impl.portal.Screenshot" = [ "gnome" ];
+      };
+    };
   };
 }
-
