670bc5c3c5a3277e505ee9240da903f403e2a6b4
Jaanai-Liu <liujaanai@gmail.com>
2026-04-18 16:22:08 +0800

    feat: add cloudflare tunnel token

--- modules/server/web-server/default.nix
+++ modules/server/web-server/default.nix
@@ -12,37 +12,40 @@ let
 in
 {
   options.modules.services.web-server = {
-    enable = mkEnableOption "114132.xyz Web Server";
+    enable = mkEnableOption "114132.xyz Web Server (via CF Tunnel)";
   };
 
   config = mkIf cfg.enable {
-    systemd.tmpfiles.rules = [
-      "d /var/www/114132_files 0755 nginx nginx - -"
-    ];
-
     services.nginx = {
       enable = true;
       virtualHosts."114132.xyz" = {
+        listen = [
+          {
+            addr = "127.0.0.1";
+            port = 80;
+          }
+        ];
         locations."/" = {
           root = "${./homepage}";
           index = "index.html";
         };
-
-        locations."/files/" = {
-          alias = "/var/www/114132_files/";
-          extraConfig = ''
-            autoindex on;
-            autoindex_exact_size off;
-            autoindex_localtime on;   
-            charset utf-8;        
-          '';
-        };
       };
     };
 
-    networking.firewall.allowedTCPPorts = [
-      80
-      443
-    ];
+    systemd.services.cloudflare-tunnel = {
+      description = "Cloudflare Tunnel for 114132.xyz";
+      wantedBy = [ "multi-user.target" ];
+      after = [ "network-online.target" ];
+      wants = [ "network-online.target" ];
+      serviceConfig = {
+        # ExecStart = "${pkgs.cloudflared}/bin/cloudflared tunnel --no-autoupdate run --token YOUR_TOKEN";
+        ExecStart = "${pkgs.cloudflared}/bin/cloudflared tunnel --no-autoupdate run --token eyJhIjoiYjA2YzE5MWIzMjA0OTRhMzQ3OTdlYTQ3MWIzYzYzYmEiLCJ0IjoiNDhiNDk0MzEtMTk0OC00NzdmLTgwYzYtYzUxNGIyMDkxYWVjIiwicyI6Ik5XSmlOMlJsTXpRdE1qQXhZaTAwTm1GaUxUbGlZbVl0TkRjd01EUmhOVE5rWldReiJ9";
+        Restart = "always";
+        RestartSec = "5s";
+        DynamicUser = true;
+      };
+    };
+
+    # networking.firewall.allowedTCPPorts = [ 80 443 ]; # 删掉或注释掉这行
   };
 }
