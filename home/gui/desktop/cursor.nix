d1706b13d643c11ae9ab70722a51f951f2c655cd
Jaanai-Liu <liujaanai@gmail.com>
2026-06-03 22:32:33 +0800

    fix cursor setting

--- home/gui/desktop/cursor.nix
+++ home/gui/desktop/cursor.nix
@@ -1,14 +1,25 @@
 # home/tui/cursor.nix
+#
+# Cursor theme & size for the whole desktop (Wayland + X11 + GTK).
+# Switch between "default" (Adwaita) and custom (Bibata) by
+# commenting/uncommenting the marked blocks.
 { pkgs, ... }:
 let
-  cursorSize = 12;
-  cursorName = "Bibata-Modern-Ice";
-  # cursorName = "Adwaita";
+  # ---- size ----
+  cursorSize = 24; # 24 = minimum usable; common: 24, 32, 48
+
+  # ---- theme (pick one) ----
+  # Default: Adwaita (built into GNOME/GTK, no extra package needed)
+  cursorName = "Adwaita";
+  cursorPackage = pkgs.adwaita-icon-theme;
+
+  # Custom: Bibata-Modern-Ice (uncomment to switch back)
+  # cursorName = "Bibata-Modern-Ice";
+  # cursorPackage = pkgs.bibata-cursors;
 in
 {
   home.pointerCursor = {
-    package = pkgs.bibata-cursors;
-    # package = pkgs.adwaita-icon-theme;
+    package = cursorPackage;
     name = cursorName;
     size = cursorSize;
     gtk.enable = true;
@@ -23,7 +34,7 @@ in
   gtk = {
     enable = true;
     cursorTheme = {
-      package = pkgs.bibata-cursors;
+      package = cursorPackage;
       name = cursorName;
       size = cursorSize;
     };
